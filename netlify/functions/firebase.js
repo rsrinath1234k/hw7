0278d5a80dfa187c0f9cc75132bedcc5974cc263
Raghav Srinath <raghav.srinath@kellogg.northwestern.com>
2021-05-17 18:08:47 -0400

    testing firebases

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,13 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyA6VXUtrvB96BEZmJiyr9IlX-fKLNDZx_Q",
+  authDomain: "kiei-451-17cd5.firebaseapp.com",
+  projectId: "kiei-451-17cd5",
+  storageBucket: "kiei-451-17cd5.appspot.com",
+  messagingSenderId: "3344532057",
+  appId: "1:3344532057:web:18a419a32ba7313f182748"} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
